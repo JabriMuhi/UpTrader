c92c1968192e94f4d1c582c4169bd20a444b4415
JabriMuhi <73785841+JabriMuhi@users.noreply.github.com>
2023-10-06 20:43:16 +0300

    feat: created menu model and registered in admin

--- menuapp/models.py
+++ menuapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='children')
+
+    def __str__(self):
+        return self.title
